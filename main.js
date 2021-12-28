759e2394a3a829fabc0851d6306de0c3aa919fff
Cella Stowitts <78512719+cstowitts@users.noreply.github.com>
2021-12-27 19:36:23 -0800

    merges local and remote repos for color randomizer mini-project

---
+++ main.js
@@ -0,0 +1,14 @@
+const letters = document.querySelectorAll(".letter");
+
+function randomRGB(){
+    const r = Math.floor(Math.random()*256);
+    const g = Math.floor(Math.random()*256);
+    const b = Math.floor(Math.random()*256);
+    return `RGB(${r}, ${g}, ${b})`;
+}
+
+setInterval(function(){
+    for(let letter of letters){
+        letter.style.color = randomRGB();
+    }
+}, 1000);
